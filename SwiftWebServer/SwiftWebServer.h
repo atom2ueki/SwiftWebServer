91b4b02ba7bbd9d7f958ebafe0d293b4a0baf323
atom2ueki <atom2ueki@gmail.com>
2020-04-22 02:40:41 +0800

    Initial Commit

---
+++ SwiftWebServer/SwiftWebServer.h
@@ -0,0 +1,19 @@
+//
+//  SwiftWebServer.h
+//  SwiftWebServer
+//
+//  Created by Tony Li on 22/4/20.
+//  Copyright © 2020 Tony Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftWebServer.
+FOUNDATION_EXPORT double SwiftWebServerVersionNumber;
+
+//! Project version string for SwiftWebServer.
+FOUNDATION_EXPORT const unsigned char SwiftWebServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftWebServer/PublicHeader.h>
+
+
---
+++ SwiftWebServerTests/SwiftWebServerTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftWebServerTests.swift
+//  SwiftWebServerTests
+//
+//  Created by Tony Li on 22/4/20.
+//  Copyright © 2020 Tony Li. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftWebServer
+
+class SwiftWebServerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
